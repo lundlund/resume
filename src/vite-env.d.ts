d3f745de3932af91d42ca00de48fd7f5c52ac6ea
Dennis Lund <lunddennis@gmail.com>
2025-10-13 18:49:05 +0200

    update Vite fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
 
-// Gør TypeScript glad for .mdx moduler
 declare module "*.mdx" {
   import type { ComponentType } from "react";
   export const meta: Record<string, unknown> | undefined;
